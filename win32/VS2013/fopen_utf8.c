4090a4954f40a96900d86d43ef5b3a6c39cdea10
taxigps <taxigps@sina.com>
2015-03-24 09:59:40 +0800

    change project to use vs2013 and move to win32/VS2013

---
+++ win32/VS2013/fopen_utf8.c
@@ -0,0 +1,9 @@
+/*
+ * This function called by freetype246MT.lib, will remove after freetype246MT.lib update.
+ */
+#include <stdio.h>
+
+FILE *fopen_utf8(const char *_Filename, const char *_Mode)
+{
+    return fopen(_Filename, _Mode);
+}
---
+++ win32/VS2013/strings.h
@@ -0,0 +1,3 @@
+#ifndef   _STRINGS_H
+#define   _STRINGS_H
+#endif
